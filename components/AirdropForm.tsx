ac66aaa2feb193dd2bc1b6baff632b4957d7b30c
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-11 23:07:36 +0400

    send airdropERC20 txn

--- components/AirdropForm.tsx
+++ components/AirdropForm.tsx
@@ -51,12 +51,39 @@ export default function AirdropForm() {
                 abi: erc20Abi,
                 address: tokenAddress as `0x${string}`,
                 functionName: "approve",
-                args: [tSenderAddress as `0x${string}`, BigInt(total)]
+                args: [tSenderAddress as `0x${string}`, BigInt(total)],
             })
             const approvalReceipt = await waitForTransactionReceipt(config, {
-                hash: approvalHash
+                hash: approvalHash,
             })
-            console.log("Approval confirmed", approvalReceipt)
+
+            console.log("Approval confirmed:", approvalReceipt)
+
+            await writeContractAsync({
+                abi: tsenderAbi,
+                address: tSenderAddress as `0x${string}`,
+                functionName: "airdropERC20",
+                args: [
+                    tokenAddress,
+                    // Comma or new line separated
+                    recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+                    amounts.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+                    BigInt(total),
+                ],
+            })
+        } else {
+            await writeContractAsync({
+                abi: tsenderAbi,
+                address: tSenderAddress as `0x${string}`,
+                functionName: "airdropERC20",
+                args: [
+                    tokenAddress,
+                    // Comma or new line separated
+                    recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+                    amounts.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+                    BigInt(total),
+                ],
+            },)
         }
     }
 
