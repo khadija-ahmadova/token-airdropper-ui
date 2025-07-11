0ad2f917254f9c7dfed3f990491e0b9d34bd0341
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-11 11:52:15 +0400

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ReactNode } from "react"
 import { Providers } from "./providers"
+import Header from "@/components/Header"
 
 export const metadata: Metadata = {
   title: "TSender",
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-
 export default function Home() {
   return (
     <div>
-      <ConnectButton />
       HI
     </div>
   );
