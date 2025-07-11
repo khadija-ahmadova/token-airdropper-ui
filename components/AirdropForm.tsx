4d30c00c5e38f5d10e3884530fb27666bfd8af7a
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-11 14:48:53 +0400

    create input form

--- components/AirdropForm.tsx
+++ components/AirdropForm.tsx
@@ -6,6 +6,13 @@ import { RiInputField } from "react-icons/ri"
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
+    const [recepients, setRecepients] = useState("")
+    const [amounts, setAmounts] = useState("")
+
+    async function handleSubmit() {
+        // TODO
+    }
+
     return (
         <div>
             <InputField
@@ -14,6 +21,23 @@ export default function AirdropForm() {
                 value={tokenAddress}
                 onChange={e => setTokenAddress(e.target.value)}
             />
+            <InputField
+                label="Recepients (comma or new line separated"
+                placeholder="0x123...,0x123...,0x123..."
+                value={tokenAddress}
+                onChange={e => setRecepients(e.target.value)}
+                large={true}
+            />
+            <InputField
+                label="Amounts (wei; comma or new line separated"
+                placeholder="100,200,300"
+                value={tokenAddress}
+                onChange={e => setAmounts(e.target.value)}
+                large={true}
+            />
+            <button onClick={handleSubmit}>
+                Send tokens
+            </button>
         </div>
     )
 }
