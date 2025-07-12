da2f8c5e505cc5e92a720bdd8d72f69f11b980e1
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-13 00:41:11 +0400

    chore: add static export settings to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  basepath: "",
+  assetPrefix: "./",
+  trailingSlash: true
 };
 
 export default nextConfig;
