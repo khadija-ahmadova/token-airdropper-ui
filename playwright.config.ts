5581246280e962260a25253adb01b8d3c7a109a3
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-13 00:15:45 +0400

    add synpress test

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   workers: process.env.CI ? parseInt(`${process.env.WORKERS}`, 10) : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 120_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- test/playwright/basic.spec.ts
+++ test/playwright/basic.spec.ts
@@ -1,15 +1,29 @@
-import { test, expect } from '@playwright/test';
+import basicSetup from '../wallet-setup/basic.setup'
+import { testWithSynpress } from '@synthetixio/synpress'
+import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright'
 
+const test = testWithSynpress(metaMaskFixtures(basicSetup))
+const { expect } = test
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test('should show mock token in token box', async ({ context, page, metamaskPage, extensionId }) => {
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId)
+  await page.goto('/')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/TSender/);
-});
+  await page.getByTestId('rk-connect-button').click()
+  await page.getByTestId('rk-wallet-option-io.metamask').waitFor({
+    state: 'visible',
+    timeout: 30000
+  });
+  await page.getByTestId('rk-wallet-option-io.metamask').click();
+  await metamask.connectToDapp();
 
-test("should dsiplay the input form when wallet connected, otherwise, not", async ({ page }) => {
-  // check for "please connect a wallet..."
-  await page.goto('/');
-  await expect(page.getByText('Please connect a wallet')).toBeVisible();
+  const customNetwork = {
+    name: 'Anvil',
+    rpcUrl: 'http://127.0.0.1:8545',
+    chainId: 31337,
+    symbol: 'ETH',
+  }
+  await metamask.addNetwork(customNetwork);
+
+  await expect(page.getByText("Token Address")).toBeVisible();
 })
